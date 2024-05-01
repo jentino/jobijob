d6780284b2e4dc94ea80fcacc70dcc056abbbeb9
jentino <jentino@gmail.com>
2024-05-01 23:23:01 +0200

    sadf
    asdfas

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express =require('express');
 const app = express();
 
-app.listen(3000, ()=>{
+app.listen(80, ()=>{
     console.log('Server is listening on port 3000');
+});
+
+app.get('/', (req, res)=>{
+    res.send('Hello World');
 });
