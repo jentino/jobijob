f14d04b69843d0f4a58b705af59ca90e3f2eb177
jentino <jentino@gmail.com>
2024-05-02 00:04:10 +0200

    ijij

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ app.listen(80, ()=>{
     console.log('Server is listening on port 80');
 });
 
+let jobs = [
+    {
+        id: 1, 
+        title: 'Web Developer',
+        description: 'Code websites',
+        salary: 80000
+    },
+    {
+        id: 2, 
+        title: 'Web Designer',
+        description: 'Design websites',
+        salary: 90000
+    },{
+        id: 3, 
+        title: 'Senior Web Developer',
+        description: 'Oversee Build websites',
+        salary: 120000
+    }
+];
+
+app.get('/jobs', (req, res)=>{
+    res.send(jobs);
+});
+
 app.get('/', (req, res)=>{
     res.send('Hello JobiJobber. Welcome here!');
 });
