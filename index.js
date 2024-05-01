ed2f313a27d3fd9e0a0e989182590e7d8e0e6cdd
jentino <jentino@gmail.com>
2024-05-01 23:25:23 +0200

    console.log('Server is listening on port 3000');

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express =require('express');
 const app = express();
 
 app.listen(80, ()=>{
-    console.log('Server is listening on port 3000');
+    console.log('Server is listening on port 80');
 });
 
 app.get('/', (req, res)=>{
