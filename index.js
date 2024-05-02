9fe2c25790a54b52a23a9c3a25cc36f9742d61c2
jentino <jentino@gmail.com>
2024-05-02 04:27:15 +0200

    carr

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let jobs = [
         id: 1, 
         title: 'Web Developer',
         description: 'This job requires someone to Code websites.',
+        longdescription: "Develop a robust and scalable website to support an app with over 1 million lines of code, ensuring high performance and reliability.  Design and implement customized landing pages for each industry we serve, reflecting their unique needs and characteristics.  Create additional landing pages for various marketing and operational purposes, ensuring brand consistency and message alignment.  Ensure responsive and optimized web design across various devices and platforms.  Integrate a flexible and powerful CMS to manage diverse content efficiently.  Provide ongoing maintenance, updates, and enhancements to the website, addressing complex challenges and ensuring seamless user experience.",
         department: 'Department of Code websites',
         salary: 80000
     },
@@ -17,12 +18,14 @@ let jobs = [
         id: 2, 
         title: 'Web Designer',
         description: 'This job requires someone to Design websites.',
+        longdescription: "Design a robust and scalable website to support an app with over 1 million lines of code, ensuring high performance and reliability.  Design and implement customized landing pages for each industry we serve, reflecting their unique needs and characteristics.  Create additional landing pages for various marketing and operational purposes, ensuring brand consistency and message alignment.  Ensure responsive and optimized web design across various devices and platforms.  Integrate a flexible and powerful CMS to manage diverse content efficiently.  Provide ongoing maintenance, updates, and enhancements to the website, addressing complex challenges and ensuring seamless user experience.",
         department: 'Department of design websites',
         salary: 90000
     },{
         id: 3, 
         title: 'Senior Web Developer',
         description: 'This job requires someone to Oversee Build websites!',
+        longdescription: "Oversee a robust and scalable website to support an app with over 1 million lines of code, ensuring high performance and reliability.  Design and implement customized landing pages for each industry we serve, reflecting their unique needs and characteristics.  Create additional landing pages for various marketing and operational purposes, ensuring brand consistency and message alignment.  Ensure responsive and optimized web design across various devices and platforms.  Integrate a flexible and powerful CMS to manage diverse content efficiently.  Provide ongoing maintenance, updates, and enhancements to the website, addressing complex challenges and ensuring seamless user experience.",
         department: 'Department of oversee websites',
         salary: 120000
     }
