be66c1e50fe4c4f6745952852f842d9003e1e3eb
jentino <jentino@gmail.com>
2024-05-02 03:56:35 +0200

    varvariants: [

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ let jobs = [
     {
         id: 1, 
         title: 'Web Developer',
-        description: 'Code websites',
+        description: 'This job requires someone to Code websites',
         department: 'Department of Code websites',
         salary: 80000
     },
     {
         id: 2, 
         title: 'Web Designer',
-        description: 'Design websites',
+        description: 'This job requires someone to Design websites',
         department: 'Department of design websites',
         salary: 90000
     },{
         id: 3, 
-        title: 'Senior Web Developer - Web Developer - Web Manager',
-        description: 'Oversee Build websites',
+        title: 'Senior Web Developer',
+        description: 'This job requires someone to Oversee Build websites',
         department: 'Department of oversee websites',
         salary: 120000
     }
